f0af02d8f225a1e781085ae992e4bd90d4f93d43
359106 <s359106@oslomet.no>
2024-02-26 16:02:19 +0100

    Har endret bakgrunnsfarge, padding, eksempeltekst og bakgrunnsbilde

--- app/page.tsx
+++ app/page.tsx
@@ -36,30 +36,33 @@ export default function Home() {
 
   return (
       <body className="new-style page-proxiedContentWrapper pageType-ContentPage template-pages-layout-landingLayout2Page pageLabel-proxiedContentWrapper smartedit-page-uid-proxiedContentWrapper smartedit-page-uuid-eyJpdGVtSWQiOiJwcm94aWVkQ29udGVudFdyYXBwZXIiLCJjYXRhbG9nSWQiOiJjbkNvbnRlbnRDYXRhbG9nIiwiY2F0YWxvZ1ZlcnNpb24iOiJPbmxpbmUifQ== smartedit-catalog-version-uuid-cnContentCatalog/Online language-no">
-  <main className="mm-page mm-slideout bg-primary-100">
-        <div className="w-full">
-          <div className="c-site-header"> {/*Navbar opplegg*/}
-            <div className="container">
-              <div className="c-site-header__top text-white text-2xl">
-                <h1>Visualiseringsverktøy</h1>
-              </div>
+      <header>
+        <div className="c-site-header"> {/*Navbar opplegg*/}
+          <div className="container">
+            <div className="c-site-header__top text-white text-2xl">
+              <h1></h1>
             </div>
           </div>
+        </div>
+      </header>
+
+      <main className="main-responsive-padding px-20 mm-page mm-slideout bg-primary-100">
+        <div className="w-full">
+
 
           <div id="__next">
             <div id="next-app-element" className="next-content-wrapper">
-              <div className="py-8 sm:py-10 md:py-14 px:4 relative">
+              <div className="py-8 sm:py-10 md:py-14 relative">
                 <div className="top-0 absolute w-full h-[calc(100%-32px)] sm:h-[calc(100%-40px)] md:h-[calc(100%-56px)] bg-jernia-image">
-                  <div className="relative z-10 mx-auto px-4 md:px-8 xl:container">
+                  <div className="relative z-10 mx-2">
                      <div className="grid grid-cols-1 md:grid-cols-12 gap-6 md:gap-8 items-center">
-                          <div className="md:col-span-10 lg:col-span-12">{/*Info området*/}
-                            <h1 className="text-3xl">Fargeradgiver</h1>
+                          <div className="pt-16 md:col-span-10 lg:col-span-12">{/*Info området*/}
+                            <h1 className="text-5xl md:text-6xl lg:text-6xl font-bold">Visualiseringsverktøy</h1>
                               <div className="mt-1 md:mt-2 max-w-4xl">
-                              <p className="leading-p text-p-lg-m md:text-p-lg">
-                                Lorem impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem
-                                impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem
-                                impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem impsumLorem
-                                impsumLorem impsumLorem impsumLorem impsumLorem
+                              <p className="pt-6 leading-p text-xl md:text-2xl lg:3xl">
+                                La deg inspirere av Jotuns fantastiske fargeunivers.
+                                Finn fargene som passer best til din stil og last opp bilde av rommet du vil male.
+                                Etter at bildet er lastet opp kan du enkelt endre veggfargen til den fargen du ønsker.
                               </p>
                               </div>
                           </div>
@@ -73,7 +76,7 @@ export default function Home() {
           <div className="px:4 bg-primary-300 py-8 md:py-14">
 
           </div>
-          <div className="flex flex-wrap w-full justify-around items-start bg-primary-100">
+          <div className="pt-24 flex flex-wrap justify-around items-start bg-primary-100">
             <button
                 className={`px-4 py-2 order-2 lg:hidden ${visibleModule === "modul2" ? "bg-blue-700" : "bg-blue-500"} text-white rounded`}
                 onClick={() => setVisibleModule("modul2")}>
@@ -90,7 +93,7 @@ export default function Home() {
             </button>
 
 
-            <div className="lg:w-1/3 w-full lg:order-1 order-2 px-2 mb-4 bg-primary-300">
+            <div className="lg:w-1/3 w-full lg:order-1 order-2 px-2 pt-6 mb-4 bg-primary-300">
               <div className={`relative pb-[100%] ${visibleModule === "modul2" ? "" : "hidden"} lg:block`}>
                 <div className={`absolute top-0 left-0 right-0 bottom-0 bg-white rounded-lg shadow p-4 overflow-hidden`} >
                   <ImageGridCard onPictureSelect={handleImageSelect}/>
@@ -98,7 +101,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="lg:w-1/3 w-full lg:order-3 order-2 px-2 mb-4">
+            <div className="lg:w-1/3 w-full lg:order-3 order-2 px-2 pt-6 mb-4">
 
               <div className={`relative pb-[100%] ${visibleModule === "modul3" ? "" : "hidden"} lg:block`}>
                 <div className={`absolute top-0 left-0 right-0 bottom-0 bg-white rounded-lg shadow p-4 overflow-hidden`}>
@@ -109,7 +112,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="lg:w-1/3 w-full lg:order-2 order-1 px-2 mb-4">
+            <div className="lg:w-1/3 w-full lg:order-2 order-1 pt-6 px-2 mb-4">
               <div className="relative pb-[100%]">
                 <div className="absolute top-0 left-0 right-0 bottom-0 bg-white rounded-lg shadow  overflow-hidden">
                   {/* The below section is dimmed until the image is loaded */}
